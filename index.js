fcad474298cc43f71196f9b2e5ea1aeea4ae29e8
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-22 20:22:25 +0100

    console.log(canvas)

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const offscreen = document.querySelector("canvas.playground").transferControlToOffscreen();
-const worker = new Worker("canvasWorker.js");
+const worker = new Worker("canvasWorker.js", {type: "module"});
 worker.postMessage({canvas: offscreen}, [offscreen]);
