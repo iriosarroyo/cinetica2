950d2bcab7e5144d220a5b2f52241005afa7acf1
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-23 13:02:32 +0100

    Added directional line

--- drawer.js
+++ drawer.js
@@ -1,7 +1,15 @@
 export const drawMolecule = (molecule, ctx) =>{
-    const {x, y, r} = molecule;
+    const {x, y, r, velX, velY} = molecule;
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI*2);
     ctx.stroke();
     ctx.fill();
+    ctx.moveTo(x, y);
+    const angle = Math.atan(Math.abs(velY/velX))
+    const distX = 1.5 * r * Math.cos(angle);
+    const distY = 1.5 * r * Math.sin(angle);
+    const endX = velX < 0 ? x - distX : x + distX;
+    const endY = velY < 0 ? y - distY : y + distY;
+    ctx.lineTo(endX, endY);
+    ctx.stroke();
 }
