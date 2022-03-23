819bfcf9f13c3d8f4f1181568a25521792c7b83c
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-23 11:41:05 +0100

    drawer check

--- canvasWorker.js
+++ canvasWorker.js
@@ -3,7 +3,6 @@ import { drawMolecule } from "./drawer.js";
 let cv, ctx;
 const messageListener = (event) => {
     const {canvas, msg, molecules, h, w} = event.data;
-    console.log(event.data)
     if(msg === "start"){
         cv = canvas;
         ctx = canvas.getContext("2d");
--- drawer.js
+++ drawer.js
@@ -1,4 +1,5 @@
 export const drawMolecule = (molecule, ctx) =>{
+    console.log(molecule)
     const {x, y, r} = molecule;
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI*2);
