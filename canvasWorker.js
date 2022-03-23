62214a7d208bfa66df283200438d5bf189b9834e
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-23 20:43:10 +0100

    performance of worker

--- canvasWorker.js
+++ canvasWorker.js
@@ -8,6 +8,7 @@ let cv, fps = calculateFPScreator(10), molecules2Draw, physicsFPS;
 let ctx;
 
 const draw = () =>{
+    console.time("initial")
     ctx.clearRect(0, 0, cv.width, cv.height);
     ctx.font = "15px Arial";
     ctx.strokeStyle = 'rgb(0,128,128)';
@@ -15,14 +16,18 @@ const draw = () =>{
     molecules2Draw.forEach((molecule) =>{
         fillStyles.set(molecule.fillStyle, [...(fillStyles.get(molecule.fillStyle) ?? []), molecule])
     })
+    console.timeEnd("initial")
+    console.time("render")
     fillStyles.forEach((value, idx)=>{
         ctx.fillStyle = idx;
         value.forEach((molecule) => drawMolecule(molecule, ctx))
     })
-    
+    console.timeEnd("render")
+    console.time("text")
     ctx.fillStyle = "black";
     ctx.fillText(fps(), cv.width - 26, 20);
     ctx.fillText(physicsFPS, cv.width - 26, 40);
+    console.timeEnd("text")
     requestAnimationFrame(draw)
 }
 
