9989f12e020ec5a67c2e4319455a8323e9098dd4
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-23 13:49:17 +0100

    Updated to random initial location

--- physics.js
+++ physics.js
@@ -1,5 +1,5 @@
 export const updateMoleculePosition = (molecule) => {
-    const {x = 0, y = 0, velX, velY, r} = molecule;
+    const {x = Math.random() * 1000, y = Math.random()*600, velX, velY, r} = molecule;
     return {
        ...molecule,
         x: x + velX,
