c862207bff93ada55a7051e276ecfa81772ec368
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-23 10:33:34 +0100

    update molecules

--- canvasWorker.js
+++ canvasWorker.js
@@ -1,26 +1,20 @@
 import { drawMolecule } from "./drawer.js";
 
-const startAnimation = (canvas, ctx) =>{
-    const molecules = Array(100).fill({r:15, velX:1, velY:1}).map(updateMoleculePosition)
-    .map(molecule => checkWallCollisions(molecule, { minX: 0, minY: 0, maxX:canvas.width, maxY:canvas.height}))
-    molecules.forEach(molecule => drawMolecule(molecule, ctx));
-    console.log("hola")
-}
-
 const cv, ctx;
 const messageListener = (event) => {
     const {canvas, msg, molecules, h, w} = event.data;
+    console.log(event.data)
     if(msg === "start"){
         cv = canvas;
         ctx = canvas.getContext("2d");
     }
     if(msg === "draw"){
-        molecules.forEach(molecules => drawMolecule(molecule, ctx));
+        molecules.forEach(molecule => drawMolecule(molecule, ctx));
     }
     if(msg === "update"){
         cv.height = h;
         cv.width = w;
     }
 }
-
+console.log("worker");
 addEventListener("message", messageListener);
--- index.js
+++ index.js
@@ -9,4 +9,9 @@ window.addEventListener("resize", () => worker.postMessage({h:window.innerHeight
 let molecules = Array(100).fill({r:15, velX:1, velY:1}).map(updateMoleculePosition)
 .map(molecule => checkWallCollisions(molecule, { minX: 0, minY: 0, maxX:window.innerWidth, maxY:window.innerHeight}))
 
+const mainLoop = () =>{
+    molecules = molecules.map(updateMoleculePosition)
+    .map(molecule => checkWallCollisions(molecule, { minX: 0, minY: 0, maxX:window.innerWidth, maxY:window.innerHeight}))
+}
+setInterval(mainLoop)
 window.requestAnimationFrame(()=> window.postMessage({msg:"draw", molecules}))
