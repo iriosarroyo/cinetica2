0904fabbeb31871a1924dcb428f031f7793dcc0d
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-24 17:51:31 +0100

    Testing calls to draw

--- canvasWorker.js
+++ canvasWorker.js
@@ -11,34 +11,26 @@ let ctx, counter = 0;
 
 const draw = () =>{
     const send = counter % 6 === 0;
-    send && startTimer("restart");
-    console.time("restart");
+    startTimer("restart");
     ctx.clearRect(0, 0, cv.width, cv.height);
     ctx.strokeStyle = 'rgb(0,128,128)';
     endTimer("restart", send);
-    send && console.timeEnd("restart");
 
     startTimer("render");
-    send && console.time("render");
     for(let i = 0; i< moleculesByStyle.length; i++){
         const [fillStyle, moleculesInGroup] = moleculesByStyle[i];
         ctx.fillStyle = fillStyle;
         for(let j = 0; j<moleculesInGroup.length; j++) drawMolecule(moleculesInGroup[j], ctx);
     }
     endTimer("render", send);
-    send && console.timeEnd("render");
 
     startTimer("fps");
-    send && console.time("fps");
     fps();
     endTimer("fps", send);
-    send && console.timeEnd("fps");
     
     startTimer("request");
-    send && console.time("request");
     requestAnimationFrame(draw)
     endTimer("request", send);
-    send && console.timeEnd("request");
     counter++;
 }
 
@@ -51,7 +43,10 @@ const messageListener = (event) => {
     if(msg === "draw"){
         moleculesByStyle = molecules;
         //physicsFPS = phFPS;
-        if(first) draw();
+        if(first) {
+            draw();
+            console.log("called draw")
+        }
     }
     if(msg === "update"){
         cv.height = h;
