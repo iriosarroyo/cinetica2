62d8939f02c6ee5795aa196742b16afee99ed212
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-24 19:21:38 +0100

    Deleted ids

--- canvasWorker.js
+++ canvasWorker.js
@@ -3,7 +3,7 @@ import { endTimer } from "./helper.js";
 import { startTimer } from "./helper.js";
 import { calculateFPScreator } from "./helper.js";
 
-let cv, fps = calculateFPScreator(10, true), moleculesByStyle, physicsFPS, ID;
+let cv, fps = calculateFPScreator(10, true), moleculesByStyle, physicsFPS;
 /**
  * @type {CanvasRenderingContext2D}
 */
@@ -14,7 +14,7 @@ const draw = () =>{
     startTimer("restart");
     ctx.clearRect(0, 0, cv.width, cv.height);
     ctx.strokeStyle = 'rgb(0,128,128)';
-    endTimer("restart", send, id);
+    endTimer("restart", send);
 
     startTimer("render");
     for(let i = 0; i< moleculesByStyle.length; i++){
@@ -22,24 +22,23 @@ const draw = () =>{
         ctx.fillStyle = fillStyle;
         for(let j = 0; j<moleculesInGroup.length; j++) drawMolecule(moleculesInGroup[j], ctx);
     }
-    endTimer("render", send, id);
+    endTimer("render", send);
 
     startTimer("fps");
     fps();
-    endTimer("fps", send, id);
+    endTimer("fps", send);
     
     startTimer("request");
     requestAnimationFrame(draw)
-    endTimer("request", send, id);
+    endTimer("request", send);
     counter++;
 }
 
 const messageListener = (event) => {
-    const {canvas, msg, molecules, h, w, first, id} = event.data;
+    const {canvas, msg, molecules, h, w, first} = event.data;
     if(msg === "start"){
         cv = canvas;
         ctx = canvas.getContext("2d");
-        ID = id;
     }
     if(msg === "draw"){
         moleculesByStyle = molecules;
